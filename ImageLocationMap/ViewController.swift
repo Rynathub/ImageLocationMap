03639eb01b7d32f744fcfc3bad22fc70609d8e6b
Rynathub <shakirovryn@gmail.com>
2025-02-10 19:40:24 +0200

    Initial Commit

---
+++ ImageLocationMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageLocationMap
+//
+//  Created by Rynat Shakirov on 10.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
