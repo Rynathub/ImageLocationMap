d5222e86bb3a144c11f3455dd780358d02e7d464
Rynathub <shakirovryn@gmail.com>
2025-02-12 17:56:34 +0200

    Initial set up of MapService with func for creating annotations

--- ImageLocationMap/Services /MapService.swift
+++ ImageLocationMap/Services /MapService.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import MapKit
+
+
+class MapService {
+    static let shared = MapService()
+    private init() {}
+    
+   func updateLocationOnMap(location: CLLocation,mapView: MKMapView,with title:String?) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location.coordinate
+        annotation.title = title
+        mapView.addAnnotation(annotation)
+        
+        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 100, longitudinalMeters: 100)
+        mapView.setRegion(region, animated: true)
+    }
+}
