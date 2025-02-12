afa7c84715ce2f92355225ffb1322f87b4d35e99
Rynathub <shakirovryn@gmail.com>
2025-02-12 18:00:43 +0200

    Implemented logic for fetching location func for checking  status & showing users choice

--- ImageLocationMap/Services /UserLocationManager.swift
+++ ImageLocationMap/Services /UserLocationManager.swift
@@ -14,6 +14,8 @@ class UserLocationManager:NSObject, CLLocationManagerDelegate {
     static let shared = UserLocationManager()
     private var locationManager: CLLocationManager?
     
+    private var currentLocation: CLLocation?
+    
     private override init() {
         super.init()
         locationManager = CLLocationManager()
@@ -24,5 +26,39 @@ class UserLocationManager:NSObject, CLLocationManagerDelegate {
     }
     func requestLocationPermission() {
         locationManager?.requestWhenInUseAuthorization()
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+                case .notDetermined:
+                    print("When user did not yet determined")
+                case .restricted:
+                    print("Restricted by parental control")
+                case .denied:
+                    print("When user select option Dont't Allow")
+                // 1
+                case .authorizedAlways:
+                    print("When user select option Change to Always Allow")
+                    locationManager?.startUpdatingLocation()
+                case .authorizedWhenInUse:
+                    print("When user select option Allow While Using App or Allow Once")
+                    // 2
+                    locationManager?.startUpdatingLocation()
+                default:
+                    print("default")
+                }
+    }
+    func getLocation() -> CLLocation? {
+        if let currentLocation {
+            return currentLocation
+        } else {
+            print("Something went wrong")
+        }
+        return nil
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let latestLocation = locations.last else { return }
+        
+        currentLocation = latestLocation
     }
 }
