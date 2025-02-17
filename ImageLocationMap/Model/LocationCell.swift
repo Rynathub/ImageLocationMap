5ea9f9872531e3e8829c520ba001559e76ad86e5
Rynathub <shakirovryn@gmail.com>
2025-02-17 22:39:35 +0200

    Implemented locationCell for tableView

--- ImageLocationMap/Model/LocationCell.swift
+++ ImageLocationMap/Model/LocationCell.swift
@@ -9,15 +9,10 @@ import UIKit
 
 class LocationCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    @IBOutlet var label: UILabel!
+    
+    func configure(location: String) {
+        label.text = location
     }
 
 }
